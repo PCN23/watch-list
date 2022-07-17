7164104674ddf1595fad04a5b57e7fa356407ba9
Pedro <pedro.chn01@gmail.com>
2022-07-17 13:46:18 -0700

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [title, setTitle] = useState('');
+  const [director, setDirector] = useState('');
+  const [color, setColor] = useState('blue');
+  const [filter, setFilter] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const movie = {
+      title: title, 
+      director: director,
+      color: color,
+    };
+    
+    const updatedMovies = [...movies, movie];
+
+    setMovies(updatedMovies);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form-and-preview'>
+        <div className="movie-form">
+          <form onSubmit={handleSubmit}>
+            <label> title
+              <input value={title} onChange={e => setTitle(e.target.value)}/>
+            </label>
+            <label> director
+              <input value={director} onChange={e => setDirector(e.target.value)}/>
+            </label>
+            <label> color
+              <select value={color} onChange={e => setColor(e.target.value)}>
+                <option value="red">rojo</option>
+                <option value="green">verde</option>
+                <option value="lightblue">azul</option>
+                <option value="cyan">azul brillante</option>
+
+              </select>
+              
+            </label>
+            <button>Add to watchlist</button>
+          </form>
+        </div>
+        <div className='movie-preview'>
+          <div className='poster' style={{ background: color }}>
+            <h2>{title}</h2>
+            <p>By {director}</p>
+          </div>
+        </div>
+      </div>
+      <div className='filter-and-list'>
+        <div className='movie-list'>
+          {
+            movies.map((movie, i) => <div key={movie.title + i} 
+              className='poster' style={{ background: movie.color }}>
+              <h2>{movie.title}</h2>
+              <p>By {movie.director}</p>
+            </div>)
+          }
+        </div>
+      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
